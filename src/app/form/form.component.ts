8257380cbf50a4273ad59bc618b1dab3247b44b2
48lexR <cbull358@gmail.com>
2023-10-26 11:27:18 -0400

    THIS WORKS.

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { Firestore, getFirestore, where } from '@angular/fire/firestore';
+import { Firestore, getFirestore, where, collection } from '@angular/fire/firestore';
 import { FormControl } from '@angular/forms';
-import { collection, query } from 'firebase/firestore';
+import { query } from 'firebase/firestore';
 import { environment } from '../../environments/environment';
 import { initializeApp } from '@angular/fire/app';
 
@@ -21,16 +21,17 @@ export class FormComponent {
   pwordc: FormControl = new FormControl('');
   uname: string = "";
   pword: string = "";
-  db: Firestore = getFirestore(this.cheese);
+  db: Firestore = getFirestore();
   usrsRef: any;
   constructor(public firestore: Firestore){ }
 
   onsubmit(){
     this.uname = this.unamec.getRawValue();
     this.pword = this.pwordc.getRawValue();
+    // console.log(`Uname: ${this.uname}\nPword: ${this.pword}`);
 
     this.usrsRef = query(
-      collection(this.db, 'usrs'),
+      collection(this.db, '/usrs'),
       where('email', '==', this.uname),
       where('password', '==', this.pword)
     );
