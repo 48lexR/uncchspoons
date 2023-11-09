41417a7e5296d2e7b89a3393cbc3afd9176a52b3
48lexR <cbull358@gmail.com>
2023-11-09 09:29:35 -0500

    Testing

--- src/app/firestore.service.ts
+++ src/app/firestore.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Firestore, collection, getDocs, query, or, where, setDoc, doc } from '@angular/fire/firestore';
 import { HttpClient } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,19 +13,35 @@ export class FirestoreService {
     this.req = req;
   }
 
-  sendMessage(body: any){
-    console.log(body);
-    return this.req.post('http://uncchspoons.net/email', JSON.parse(JSON.stringify(body)), {
-      headers: {
-      'Content-Type': 'application/json'
-    }
-  }).subscribe((res) => {
-    console.log(res);
-    return res;
-  });
-}
+  /**
+   * 
+   * @param body The body of an email address as a JSON file
+   * @returns an Observable which can be subscribed
+   */
+  // sendMessage(body: any): boolean{
+  //   console.log(body);
+  //   this.req.post('http://uncchspoons.net/email', JSON.parse(JSON.stringify(body)), {
+  //     headers: {
+  //     'Content-Type': 'application/json'
+  //   }
+  //   }).subscribe({
+  //     next : v => {
+  //       console.log(v);
+  //     }, 
+  //     error : e => {
+  //       alert(e.message)
+  //       console.log(e);
+  //       return false;
+  //     }
+  //   });
+  //   return true;
+  //   }
 
-
+/**
+ * 
+ * @param _user an object with several properties
+ * @returns A promise which evaluates to either true (user registered) or false (user not registered)
+ */
   async register(_user: { name: string; uname: string; pword: string; ID: string; target: null; }): Promise<boolean>{
     let a = false;
 
@@ -40,24 +57,49 @@ export class FirestoreService {
     });
 
     if(!a) return false;
-
+    a=true;
       //implement sendmessage
 
-    this.sendMessage(_user);
-
-    await setDoc(doc(collection(this.db, "/usrs")), {
-      email: _user.uname,
-      pword: _user.pword,
-      isAdmin: false,
-      kills: 0,
-      name: _user.name,
-      target: null
+    this.req.post('http://uncchspoons.net/email', JSON.parse(JSON.stringify(_user)), {
+        headers: {
+        'Content-Type': 'application/json'
+      }
+      })
+      .subscribe({
+        next : async v => {
+          console.log(v);
+          return setDoc(doc(collection(this.db, "/usrs")), {
+            email: _user.uname,
+            pword: _user.pword,
+            isAdmin: false,
+            kills: 0,
+            name: _user.name,
+            target: null
+          }).then(res => {
+            return true;
+          }).catch(err=>{
+            alert(err.message);
+            return false;
+          });
+      
+        }, 
+        error : e => {
+          alert("Failed to send email: " + e.message);
+          console.log(e);
+          return false;
+        }
       });
+      return false;
+      
 
-      return true;
 
     }
 
+/**
+ * @param user a user argument to be submitted
+ * @param date current time
+ * @param target target killed (defaults to null)
+ */
   async click(user: { name: string; uname: string; pword: string; kills: number; isAdmin: boolean; target: null; }, date: Date, target: null) {
     await setDoc(doc(collection(this.db, "/kill")), {
       name: user,
@@ -66,6 +108,13 @@ export class FirestoreService {
     })
   }
 
+  /**
+   * 
+   * @param uname the username (email) of a hypothetical user
+   * @param pword the password (pword) of a hypothetical user
+   * @returns true (user exists/logged in) or false (user does not exist/not logged in)
+   * @throws TypeError: not a string (for either uname or pword)
+   */
   async onSubmit(uname: any, pword: any): Promise<boolean> {
     if(typeof uname !== "string") throw new TypeError("uname is not a string!!!");
     if(typeof pword !== "string") throw new TypeError("pword is not a string!!!");
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -13,9 +13,11 @@ import { FirestoreService } from '../firestore.service';
 export class RegisterComponent {
 
   register = new FormGroup({
-    namec: new FormControl("", Validators.required),
+    namec: new FormControl("",[ Validators.required, Validators.pattern(
+      new RegExp("(.*) (.*)"))
+    ]),
     unamec: new FormControl("", [Validators.required, Validators.pattern(
-      /(.*)@(.*)unc.edu/g
+      new RegExp("(.*)@(.*)unc.edu")
     )])
   });
   user = {
@@ -29,14 +31,24 @@ export class RegisterComponent {
   constructor(private fs: FirestoreService) { }
 
   async submit(){
+    if(
+      this.register.controls["unamec"].errors || 
+      this.register.controls['namec'].errors) 
+      {
+        alert(`Name ${this.register.controls['namec'].value} or Username ${this.register.controls['unamec'].value} invalid. Make sure to type your first and last name, then your @unc.edu email.`);
+        console.log(`Username ${this.register.controls["unamec"].value} error`);
+        console.log(`Name ${this.register.controls["namec"].value} error`);
+        return;
+      }
     this.user.name = String(this.register.controls['namec'].value);
     this.user.uname = String(this.register.controls['unamec'].value);
+    console.log(this.user.uname);
     this.user.pword = (Math.random()*256).toString(16);
 
     let a = await this.fs.register(this.user);
 
     if(a) alert("An email has been sent to your account");
-    else alert("Sorry, that user already exists.")
+    else alert("Sorry, your request to register could not be completed.")
   }
     //make sure this user DOESN'T already exist
 
